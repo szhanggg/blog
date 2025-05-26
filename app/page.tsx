b0ae2dd5222d5b426e28cee516f9569e9ba0b53a
szhanggg <stevez9101@gmail.com>
2025-05-26 19:07:18 -0400

    day one

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,11 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Stephen's Daily Blog
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm a Computer Science student at Carnegie Mellon University, interested in
+          trying new things and getting the most out of my life!`}
       </p>
       <div className="my-8">
         <BlogPosts />
